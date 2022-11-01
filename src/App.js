94d87aac759947aa9e592d96ace107012565d489
Karim Hamga <karimhamga@macbook-pro-de-karim-1.home>
2022-11-01 14:01:48 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import logo from "./images/logo.webp";
+import data from "./data.json";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img className="logo" src={logo} alt="Logo Netflix" />
+      {data.map((elem, index) => {
+        return (
+          <div className="container">
+            key={index}
+            <p className="category">{elem.category}</p>
+            <div className="movie-list">
+              {elem.images.map((elem) => {
+                return <img src={elem} alt="movie" />;
+              })}
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
-}
+};
 
 export default App;
